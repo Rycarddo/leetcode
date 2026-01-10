390e55fb51551b1a41874717bf27ae07db9b664d
Rycarddo <joserycarddo2003@gmail.com>
2026-01-10 17:27:06 -0300

    f

---
+++ easy/9.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        x = list(str(x))
+        reversed_x = list(reversed(x))
+
+        return x == reversed_x
