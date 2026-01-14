6fffea787ad83100587d9834bc1df3b57ed455c1
Rycarddo <joserycarddo2003@gmail.com>
2026-01-13 22:31:48 -0300

    the real day 3

---
+++ 0-easy/13.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+
+        romans = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000,
+        }
+
+        n = len(s)
+        res = 0
+
+        for i in range(n):
+            if i + 1 < n and romans[s[i]] < romans[s[i + 1]]:
+                res -= romans[s[i]]
+            else:
+                res += romans[s[i]]
+
+        return res
