5e4a36755f368b91f003c174512e675187f0e109
Rycarddo <joserycarddo2003@gmail.com>
2026-01-16 21:53:57 -0300

    updating

--- 0-easy/3512.java
+++ 0-easy/3512.java
@@ -1,14 +1,6 @@
 class Solution {
     public int minOperations(int[] nums, int k) {
-        int big = nums[0];
-        int idx = 0;
         int counter = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] > big) {
-                big = nums[i];
-                idx = i;
-            };
-        };
 
         int sum = 0;
 
@@ -17,14 +9,8 @@ class Solution {
         };
 
         while (sum % k != 0) {
-            nums[idx]--;
+            sum--;
             counter++;
-            sum = 0;
-
-            for (int num : nums) {
-            sum += num;
-            };
-
         };
         
         return counter;
