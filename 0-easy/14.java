b3fd2a5d98211cb016c33a34d00e5ca917cdeeaf
Rycarddo <joserycarddo2003@gmail.com>
2026-01-18 21:20:19 -0400

    day 8

---
+++ 0-easy/14.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        int n = strs.length;
+
+        int size = 0;
+
+        // taking the size of the smallest string
+        for (int i = 0; i < n; i++) {
+            String small = "";
+            
+            if (strs[i].length() < small.length()) {
+                small = strs[i];
+                size = small.length();
+                
+            };
+        };
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < )
+        }
+
+    }
+}
